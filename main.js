d06bac316596ee186ec3b1228a223ca74f12e8fb
Andrii <stavnichenkoa2000@gmail.com>
2025-10-26 15:14:57 +0200

    Update the ball to bounce off the canvas borders

--- main.js
+++ main.js
@@ -1,20 +1,47 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
+const runButton = document.getElementById("runButton");
+const addBall = document.getElementById("addBall");
+const ballRadius = 10;
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "red";
-ctx.fill();
-ctx.closePath();
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
 
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgb(0 0 255 / 0.5)";
-ctx.stroke();
-ctx.closePath();
+let x = Math.random() * canvas.width;
+let y = Math.random() * canvas.height;
+
+let dx = 2;
+let dy = -2;
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    x+= dx;
+    y+= dy;
+
+    if (y + dy < (ballRadius / 2) || y + dy > canvas.height - (ballRadius / 2)) {
+        dy = -dy;
+    }
+
+    if (x + dx > canvas.width - (ballRadius / 2) || x + dx < (ballRadius / 2)) {
+        dx = -dx;
+    }
+}
+
+function drawBall () {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath;  
+}
+
+function startGame () {
+    setInterval(draw, 10);
+}
+
+runButton.addEventListener("click", () => {
+  startGame();
+  runButton.disabled = true;
+});
+
+
